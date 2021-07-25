f5bf06338e30fd1a3791d2d910220a0d6e33fd40
likui345 <likui345@gmail.com>
2021-07-25 22:28:53 +0800

    v.0.2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='rbsa',
+    version='v0.2',
+    py_modules=['module.rbsa','module.ovlp2graph','module.graph2chr','module.chr_paths','module.filter'],
+    author = 'likui',
+    author_email = 'likui345@126.com',
+    requires = ['mummer','networkx'],
+    url='https://github.com/likui345/PGA'
+)
